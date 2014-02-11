65ea6e8eb2eb9eff0ec7147670d24e4453f7201e
Daniel Dao <danielvd@cs.utexas.edu>
2014-02-10 21:50:48 -0600

    Finished initializing the stack.

--- threads.c
+++ threads.c
@@ -2,5 +2,8 @@
 #include <threads.h> 
 
 struct thread{
-	*uni_ptr_t Stack = 0; 
+	uni_ptr_t* stack;
+
+	//Allocates an initial with 4096 slots 
+	stack = (uni_ptr_t*)malloc(sizeof(uni_ptr_t) * 4096);
 }
