1e4b47a5cbfe0a2acd22e6d1f208a267390df3a0
Daniel Dao <danielvd@cs.utexas.edu>
2014-02-10 21:39:42 -0600

    Starting the lab...

---
+++ threads.h
@@ -0,0 +1,14 @@
+#ifndef THREADS_H_
+#define THREADS_H_
+
+struct thread;
+
+struct thread *thread_create(void (*f)(void *arg), void *arg);
+void thread_add_runqueue(struct thread *t);
+void thread_yield(void);
+void dispatch(void);
+void schedule(void);
+void thread_exit(void);
+void thread_start_threading(void);
+
+#endif // THREADS_H
