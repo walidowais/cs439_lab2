844f293760bd359f8d34ed273e4b296a1efae51b
Daniel Dao <danielvd@cs.utexas.edu>
2014-02-10 21:55:17 -0600

    Implemeneted the base pointer and the stack pointer.

--- threads.c
+++ threads.c
@@ -3,6 +3,8 @@
 
 struct thread{
 	uni_ptr_t* stack;
+	uni_ptr_t* base_pointer;
+ 	uni_ptr_t* stack_pointer;
 
 	//Allocates an initial with 4096 slots 
 	stack = (uni_ptr_t*)malloc(sizeof(uni_ptr_t) * 4096);
