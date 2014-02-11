f6f13b6a822667e05df68cffd53f33d6c701164b
Daniel Dao <danielvd@cs.utexas.edu>
2014-02-10 22:00:00 -0600

    Named uintptr_t wrong, fixed that.

--- threads.c
+++ threads.c
@@ -2,10 +2,11 @@
 #include <threads.h> 
 
 struct thread{
-	uni_ptr_t* stack;
-	uni_ptr_t* base_pointer;
- 	uni_ptr_t* stack_pointer;
+	uintptr_t* stack;
+	uintptr_t* base_pointer;
+ 	uintptr_t* stack_pointer;
 
 	//Allocates an initial with 4096 slots 
-	stack = (uni_ptr_t*)malloc(sizeof(uni_ptr_t) * 4096);
+	stack = (uni_ptr_t*)malloc(sizeof(uintptr_t) * 4096);
+
 }
